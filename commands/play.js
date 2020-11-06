45a19aa03991e75e7aec262d13bb32311bd18eec
Ian Castaño <ianfe@martina.com.co>
2020-11-05 22:34:40 -0500

    API Key removal from config

--- commands/play.js
+++ commands/play.js
@@ -3,16 +3,8 @@ const ytdl = require("ytdl-core");
 const YouTubeAPI = require("simple-youtube-api");
 const scdl = require("soundcloud-downloader");
 
-let YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID;
-try {
-  const config = require("../config.json");
-  YOUTUBE_API_KEY = config.YOUTUBE_API_KEY;
-  SOUNDCLOUD_CLIENT_ID = config.SOUNDCLOUD_CLIENT_ID;
-} catch (error) {
-  YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
-  SOUNDCLOUD_CLIENT_ID = process.env.SOUNDCLOUD_CLIENT_ID;
-}
-const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
+let SOUNDCLOUD_CLIENT_ID;
+const youtube = new YouTubeAPI(process.env.YOUTUBE_API_KEY);
 
 module.exports = {
   name: "play",
